91b843909fa04ae83d6aedf45451884d1b0e67f5
tatsuki0502 <ap2256539@outlook.jp>
2026-01-22 23:56:23 +0900

    ポートフォリオ削除

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Skills from "./sections/Skills";
-import Portfolio from "./sections/Portfolio";
 import Service from "./sections/Service";
-import Contact from "./sections/Contact";
 
 function App() {
   return (
